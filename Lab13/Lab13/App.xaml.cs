d5c1a72bc9144b846da1b9f815bae4404f48c793
Juan-Carlos123 <juan.romani@tecsup.edu.pe>
2021-01-07 23:51:51 -0500

    first commit

---
+++ Lab13/Lab13/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+using Lab13.DataContext;
+using Lab13.Interfaces;
+using Lab13.Views;
+
+
+namespace Lab13
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            GetContext().Database.EnsureCreated();
+            MainPage = new NavigationPage(new Views.MainPage());
+        }
+
+        // Método para obtener el contexto cuando se inicia la aplicación
+        public static AppDbContext GetContext()
+        {
+            string DbPath = DependencyService.Get<IConfigDataBase>().GetFullPath("efCore.db");
+
+            return new AppDbContext(DbPath);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lab13/Lab13/Models/Artista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab13.Models
+{
+    public class Artista
+    {
+        public int ArtistaID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
+
+
